48b701672773e31b36bcb3eafe1908929bfee4c2
Bushsbakedbean <114026987+Bushsbakedbean@users.noreply.github.com>
2022-10-04 16:27:22 -0400

    Update DriveConstants.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
@@ -99,7 +99,7 @@ public class DriveConstants {
      * https://github.com/acmerobotics/road-runner-quickstart/issues/91
      
      */
-    public static double MAX_VEL = 90;
+    public static double MAX_VEL = 10;
     public static double MAX_ACCEL = 90;
     public static double MAX_ANG_VEL = Math.toRadians(286.4788975654116);
     public static double MAX_ANG_ACCEL = Math.toRadians(286.4788975654116);
